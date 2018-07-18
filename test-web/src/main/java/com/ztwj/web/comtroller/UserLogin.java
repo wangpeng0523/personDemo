1f628adcac16335de2297535b4c23ed33c717e65
nickwang <wangpeng@toptolink.com>
2018-07-18 12:22:21 +0800

    新增controller

---
+++ test-web/src/main/java/com/ztwj/web/comtroller/UserLogin.java
@@ -0,0 +1,10 @@
+package main.java.com.ztwj.web.comtroller;
+
+/**
+ * Created by nickwang on 2018/7/18.
+ */
+public class UserLogin {
+    public String userLongin(){
+        return null;
+    }
+}

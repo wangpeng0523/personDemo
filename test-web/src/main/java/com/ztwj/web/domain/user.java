9ad48ac767a4b1bf090acbf6da9a8bfa2e93a05e
nickwang <wangpeng@toptolink.com>
2018-07-18 11:56:41 +0800

    修改提交

--- test-web/src/main/java/com/ztwj/web/domain/user.java
+++ test-web/src/main/java/com/ztwj/web/domain/user.java
@@ -4,4 +4,40 @@ package main.java.com.ztwj.web.domain;
  * Created by nickwang on 2018/7/18.
  */
 public class user {
+    private String username;
+    private String psw;
+    private String hobby;
+    private Integer perNum;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Integer getPerNum() {
+        return perNum;
+    }
+
+    public void setPerNum(Integer perNum) {
+        this.perNum = perNum;
+    }
 }

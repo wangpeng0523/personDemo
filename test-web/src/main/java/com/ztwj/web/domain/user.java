5060576edaa5c4c831c0336694b898dbec3d04b4
nickwang <wangpeng@toptolink.com>
2018-07-18 12:27:22 +0800

    mine修改

--- test-web/src/main/java/com/ztwj/web/domain/user.java
+++ test-web/src/main/java/com/ztwj/web/domain/user.java
@@ -3,7 +3,7 @@ package main.java.com.ztwj.web.domain;
 /**
  * Created by nickwang on 2018/7/18.
  */
-public class user {
+public class User {
     private String username;
     private String psw;
     private String hobby;
